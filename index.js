55b9e271e41f986777a09e5abd837e1ec554cb9c
PRATHIUVISWAK <prathiuviswakdev@gmail.com>
2024-02-16 21:57:05 +0530

    c2

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
+let bodyParser = require("body-parser")
 const app = express();
 const port = 3000;
-let numberofrequests = 0
-function middleware1(req,res,next){
-  numberofrequests = numberofrequests + 1
-  console.log(numberofrequests)
-  console.log(req.headers.counter)
-  //res.send("some error")
-  next()
-}
+// let numberofrequests = 0
+// function middleware1(req,res,next){
+//   numberofrequests = numberofrequests + 1
+//   console.log(numberofrequests)
+//   console.log(req.headers.counter)
+//   //res.send("some error")
+//   next()
+// }
 
-app.use(middleware1)
+
+app.use(bodyParser.json())
+//app.use(middleware1)
+
+
+
 app.get("/hello", (req, res) => {
   res.send("Hello, Express!");
 });
@@ -20,10 +26,17 @@ function cal(num) {
 }
 
 app.post("/math", (req, res) => {
-  var counter = req.headers.counter;
-  let val = cal(counter);
-  let ans = "square of 10 : " + val;
-  res.send(ans);
+  //var counter = req.headers.counter;
+  var counter = req.body.counter
+  if(counter <= 100){
+    let val = cal(counter);
+    let ans = "square of 10 : " + val;
+    res.send(ans);
+  }
+  else{
+    res.status(400).send("input exceeds 100")
+  }
+  
 });
 
 app.listen(port, () => {
